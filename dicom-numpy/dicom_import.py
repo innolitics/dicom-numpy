76e9b82dba6d5f1931e1e4401909c7aa0cb28cf6
J. David Giese <johndgiese@gmail.com>
2017-03-27 23:58:46 -0400

    Setup basic python package
    The core DICOM import code was copied from scripts we developed over the
    course of a couple projects.
    
    Note that the API is NOT stable yet, and that the code should be split
    into some smaller files.

---
+++ dicom-numpy/dicom_import.py
@@ -0,0 +1,235 @@
+import math
+import zipfile
+import logging
+import tempfile
+
+import dicom
+import numpy as np
+
+
+logger = logging.getLogger(__name__)
+
+
+class DicomImportException(Exception):
+    pass
+
+
+def combined_series_from_zip(zip_filename):
+    logger.info('Extracting voxel data from "{}"'.format(zip_filename))
+
+    if not zipfile.is_zipfile(zip_filename):
+        raise DicomImportException('Invalid zipfile {}'.format(zip_filename))
+
+    with zipfile.ZipFile(zip_filename, 'r') as zip_file:
+        datasets = dicom_datasets_from_zip(zip_file)
+
+    voxels, ijk_to_xyz = combine_slices(datasets)
+    return voxels, ijk_to_xyz
+
+
+def dicom_datasets_from_zip(zip_file):
+    datasets = []
+    for entry in zip_file.namelist():
+        if entry.endswith('/'):
+            continue  # skip directories
+
+        entry_pseudo_file = zip_file.open(entry)
+
+        # the pseudo file does not support `seek`, which is required by
+        # dicom's lazy loading mechanism; use temporary files to get around this;
+        # relies on the temporary files not being removed until the temp
+        # file is garbage collected, which should be the case because the
+        # dicom datasets should retain a reference to the temp file
+        temp_file = tempfile.TemporaryFile()
+        temp_file.write(entry_pseudo_file.read())
+        temp_file.flush()
+        temp_file.seek(0)
+
+        try:
+            dataset = dicom.read_file(temp_file)
+            datasets.append(dataset)
+        except dicom.errors.InvalidDicomError as e:
+            msg = 'Skipping invalid DICOM file "{}": {}'
+            logger.info(msg.format(entry, e))
+
+    if len(datasets) == 0:
+        raise DicomImportException('Zipfile does not contain any valid DICOM files')
+
+    return datasets
+
+
+def combine_slices(slice_datasets):
+    '''
+    Given a list of pydicom datasets for an image series, stitch them together into a
+    three-dimensional numpy array.  Also return a 4x4 affine transformation
+    matrix that converts the ijk-pixel-indices into the xyz-coordinates in the
+    patient's coordinate system.
+    See http://dicom.innolitics.com/ciods/ct-image/image-plane for details.
+    This matrix, M, should fulfill:
+    [x, y, z, 1].T = M @ [i, j, k, 1].T
+    The pixel array will be cast to floating point values.
+    This function requires that the datasets fulfill these properties:
+    - Are all part of the same series (0020,000E)
+    - The set of slices provided has no internal "gaps"; note that missing
+      slices on the ends of the dataset are not detected
+    - The size of each slice and the spacing between the pixels in the slice
+      must be the same (0020,0032)
+    '''
+    if len(slice_datasets) == 0:
+        raise DicomImportException("Must provide at least one DICOM dataset")
+
+    validate_slices_form_uniform_grid(slice_datasets)
+
+    voxels = merge_slice_pixel_arrays(slice_datasets)
+    transform = ijk_to_patient_xyz_transform_matrix(slice_datasets)
+
+    return voxels, transform
+
+
+def merge_slice_pixel_arrays(slice_datasets):
+    num_rows = slice_datasets[0].Rows
+    num_columns = slice_datasets[0].Columns
+    num_slices = len(slice_datasets)
+
+    dtype = slice_datasets[0].pixel_array.dtype
+    voxels = np.empty((num_columns, num_rows, num_slices), dtype=dtype)
+
+    sorted_slice_datasets = _sort_by_slice_spacing(slice_datasets)
+    for k, dataset in enumerate(sorted_slice_datasets):
+        try:
+            slope = float(getattr(dataset, 'RescaleSlope'))
+            intercept = float(getattr(dataset, 'RescaleIntercept'))
+            voxels[:, :, k] = dataset.pixel_array.astype(np.float32).T*slope + intercept
+        except AttributeError:
+            voxels[:, :, k] = dataset.pixel_array.T
+
+    return voxels
+
+
+def ijk_to_patient_xyz_transform_matrix(slice_datasets):
+    first_slice_dataset = _sort_by_slice_spacing(slice_datasets)[0]
+    row_cosine, column_cosine, slice_cosine = _extract_cosines(first_slice_dataset.ImageOrientationPatient)
+
+    row_spacing, column_spacing = first_slice_dataset.PixelSpacing
+    slice_spacing = _slice_spacing(slice_datasets)
+
+    transform = np.identity(4, dtype=np.float32)
+
+    transform[:3, 0] = row_cosine*column_spacing
+    transform[:3, 1] = column_cosine*row_spacing
+    transform[:3, 2] = slice_cosine*slice_spacing
+
+    transform[:3, 3] = first_slice_dataset.ImagePositionPatient
+
+    return transform
+
+
+def validate_slices_form_uniform_grid(slice_datasets):
+    '''
+    Perform various data checks to ensure that the list of slices form a
+    evenly-spaced grid of data.
+    Some of these checks are probably not required if the data follows the
+    DICOM specification, however it seems pertinent to check anyway.
+    '''
+    invariant_properties = [
+        'Modality',
+        'SOPClassUID',
+        'SeriesInstanceUID',
+        'Rows',
+        'Columns',
+        'ImageOrientationPatient',
+        'PixelSpacing',
+        'PixelRepresentation',
+        'BitsAllocated',
+        'BitsStored',
+        'HighBit',
+    ]
+
+    for property_name in invariant_properties:
+        _slice_attribute_equal(slice_datasets, property_name)
+
+    _validate_image_orientation(slice_datasets[0].ImageOrientationPatient)
+
+    slice_positions = _slice_positions(slice_datasets)
+    _check_for_missing_slices(slice_positions)
+
+
+def _validate_image_orientation(image_orientation):
+    '''
+    Ensure that the image orientation is supported
+    - The direction cosines have magnitudes of 1 (just in case)
+    - The direction cosines are perpendicular
+    - The direction cosines are oriented along the patient coordinate system's axes
+    '''
+    # TODO: deduplicate this
+    row_cosine, column_cosine, slice_cosine = _extract_cosines(image_orientation)
+
+    if not _almost_zero(np.dot(row_cosine, column_cosine), 1e-4):
+        raise DicomImportException("Non-orthogonal direction cosines: {}, {}".format(row_cosine, column_cosine))
+    elif not _almost_zero(np.dot(row_cosine, column_cosine), 1e-8):
+        logger.warn("Direction cosines aren't quite orthogonal: {}, {}".format(row_cosine, column_cosine))
+
+    if not _almost_one(np.linalg.norm(row_cosine), 1e-4):
+        raise DicomImportException("The row direction cosine's magnitude is not 1: {}".format(row_cosine))
+    elif not _almost_one(np.linalg.norm(row_cosine), 1e-8):
+        logger.warn("The row direction cosine's magnitude is not quite 1: {}".format(row_cosine))
+
+    if not _almost_one(np.linalg.norm(column_cosine), 1e-4):
+        raise DicomImportException("The column direction cosine's magnitude is not 1: {}".format(column_cosine))
+    elif not _almost_one(np.linalg.norm(column_cosine), 1e-8):
+        logger.warn("The column direction cosine's magnitude is not quite 1: {}".format(column_cosine))
+
+
+def _almost_zero(value, abs_tol):
+    return math.isclose(value, 0.0, abs_tol=abs_tol)
+
+
+def _almost_one(value, abs_tol):
+    return math.isclose(value, 1.0, abs_tol=abs_tol)
+
+
+def _extract_cosines(image_orientation):
+    row_cosine = np.array(image_orientation[:3])
+    column_cosine = np.array(image_orientation[3:])
+    slice_cosine = np.cross(row_cosine, column_cosine)
+    return row_cosine, column_cosine, slice_cosine
+
+
+def _slice_attribute_equal(slice_datasets, property_name):
+    initial_value = getattr(slice_datasets[0], property_name, None)
+    for dataset in slice_datasets[1:]:
+        value = getattr(dataset, property_name, None)
+        if value != initial_value:
+            msg = 'All slices must have the same value for "{}": {} != {}'
+            raise DicomImportException(msg.format(property_name, value, initial_value))
+
+
+def _slice_positions(slice_datasets):
+    image_orientation = slice_datasets[0].ImageOrientationPatient
+    row_cosine, column_cosine, slice_cosine = _extract_cosines(image_orientation)
+    return [np.dot(slice_cosine, d.ImagePositionPatient) for d in slice_datasets]
+
+
+def _check_for_missing_slices(slice_positions):
+    slice_positions_diffs = np.diff(sorted(slice_positions))
+    if not np.allclose(slice_positions_diffs, slice_positions_diffs[0], atol=0, rtol=1e-5):
+        # TODO: figure out how we should handle non-even slice spacing
+        msg = "The slice spacing is non-uniform. Slice spacings:\n{}"
+        logger.warn(msg.format(slice_positions_diffs))
+
+    if not np.allclose(slice_positions_diffs, slice_positions_diffs[0], atol=0, rtol=1e-1):
+        raise DicomImportException('It appears there are missing slices')
+
+
+def _slice_spacing(slice_datasets):
+    if len(slice_datasets) > 1:
+        slice_positions = _slice_positions(slice_datasets)
+        slice_positions_diffs = np.diff(sorted(slice_positions))
+        return np.mean(slice_positions_diffs)
+    else:
+        return 0.0
+
+
+def _sort_by_slice_spacing(slice_datasets):
+    slice_spacing = _slice_positions(slice_datasets)
+    return [d for (s, d) in sorted(zip(slice_spacing, slice_datasets))]
---
+++ setup.py
@@ -0,0 +1,59 @@
+"""
+A setuptools based setup module.
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dicom-numpy',
+    version='0.1.0',
+    description='Extract image data into a 3D numpy array from a set of DICOM files.',
+    long_description=long_description,
+    url='https://github.com/innolitics/dicom-numpy',
+    author='Innolitics, LLC',
+    author_email='info@innolitics.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Developers',
+	'Intended Audience :: Healthcare Industry',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='dicom numpy',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=[
+        'pydicom',
+        'numpy',
+    ],
+
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage', 'pytest'],
+    },
+
+    package_data={},
+    data_files=[],
+    entry_points={},
+)
