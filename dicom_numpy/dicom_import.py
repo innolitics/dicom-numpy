ec7cf5048c19db489542e87f7a3e1b39420b2f67
J. David Giese <johndgiese@gmail.com>
2017-03-28 13:24:15 -0400

    Split code into modules
    This is in preparation for adding more utility functions.

--- dicom_numpy/dicom_import.py
+++ dicom_numpy/dicom_import.py
@@ -1,62 +1,12 @@
-import zipfile
 import logging
-import tempfile
 
-import dicom
 import numpy as np
 
 from .utils import isclose
+from .exceptions import DicomImportException
 
 
 logger = logging.getLogger(__name__)
-
-
-class DicomImportException(Exception):
-    pass
-
-
-def combined_series_from_zip(zip_filename):
-    logger.info('Extracting voxel data from "{}"'.format(zip_filename))
-
-    if not zipfile.is_zipfile(zip_filename):
-        raise DicomImportException('Invalid zipfile {}'.format(zip_filename))
-
-    with zipfile.ZipFile(zip_filename, 'r') as zip_file:
-        datasets = dicom_datasets_from_zip(zip_file)
-
-    voxels, ijk_to_xyz = combine_slices(datasets)
-    return voxels, ijk_to_xyz
-
-
-def dicom_datasets_from_zip(zip_file):
-    datasets = []
-    for entry in zip_file.namelist():
-        if entry.endswith('/'):
-            continue  # skip directories
-
-        entry_pseudo_file = zip_file.open(entry)
-
-        # the pseudo file does not support `seek`, which is required by
-        # dicom's lazy loading mechanism; use temporary files to get around this;
-        # relies on the temporary files not being removed until the temp
-        # file is garbage collected, which should be the case because the
-        # dicom datasets should retain a reference to the temp file
-        temp_file = tempfile.TemporaryFile()
-        temp_file.write(entry_pseudo_file.read())
-        temp_file.flush()
-        temp_file.seek(0)
-
-        try:
-            dataset = dicom.read_file(temp_file)
-            datasets.append(dataset)
-        except dicom.errors.InvalidDicomError as e:
-            msg = 'Skipping invalid DICOM file "{}": {}'
-            logger.info(msg.format(entry, e))
-
-    if len(datasets) == 0:
-        raise DicomImportException('Zipfile does not contain any valid DICOM files')
-
-    return datasets
 
 
 def combine_slices(slice_datasets):
---
+++ dicom_numpy/exceptions.py
@@ -0,0 +1,2 @@
+class DicomImportException(Exception):
+    pass
