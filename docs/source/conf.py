5e558a7c27f4f53dd0660a2dd64b31031548004c
J. David Giese <johndgiese@gmail.com>
2017-04-28 14:44:10 -0400

    Mock out numpy so that read-the-docs can run `autodoc`
    http://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,18 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
+
 sys.path.insert(0, os.path.abspath('../..'))
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['numpy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- General configuration ------------------------------------------------
