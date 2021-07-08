0b62db8ab5c84a821f62c1cced5e7678697ec72e
Reece Stevens <reecestevens24@gmail.com>
2021-07-08 06:59:17 -0500

    Move version to separate module to prevent buildtime issues
    Because setup.py was importing the package in order to get the version
    parameter, it was hitting errors about missing build-time dependencies
    before it actually got to the step of installing the build-time
    dependencies.
    
    This was resolved by separating the version out into its own module,
    which is read in directly by setup.py.
    
    See the following SO post for more information:
    
    https://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package

--- dicom_numpy/__init__.py
+++ dicom_numpy/__init__.py
@@ -1,7 +1,6 @@
 from .combine_slices import combine_slices, sort_by_slice_position
 from .exceptions import DicomImportException
-
-__version__ = '0.6.0'
+from .version import __version__
 
 __all__ = [
     'combine_slices',
